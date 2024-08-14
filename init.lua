e55e774e0c8820c2760c938223a6dffb54215fe1
arcadus <arcadus@gmail.com>
2024-08-14 22:06:53 +0100

    added keybinds for copilot and bufferline, removed arrowkey binds

--- init.lua
+++ init.lua
@@ -23,3 +23,16 @@ vim.keymap.set("i", "kj", "<Esc>", {})
 vim.keymap.set("i", "jk", "<Esc>", {})
 vim.keymap.set("i", "kk", "<Esc>", {})
 vim.keymap.set("i", "jj", "<Esc>", {})
+
+-- Helper function to unbind keys in multiple modes
+local function unbind_keys(keys)
+	local modes = { "n", "i", "v" }
+	for _, mode in ipairs(modes) do
+		for _, key in ipairs(keys) do
+			vim.api.nvim_set_keymap(mode, key, "", { noremap = true, silent = true })
+		end
+	end
+end
+
+-- Unbind arrow keys
+unbind_keys({ "<Up>", "<Down>", "<Left>", "<Right>" })
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,9 @@
 return {
 	'akinsho/bufferline.nvim',
 	version = "*",
-	dependencies = 'nvim-tree/nvim-web-devicons'
+	dependencies = 'nvim-tree/nvim-web-devicons',
+	keys = {
+		{"<leader>l", "<cmd>BufferLineCycleNext<cr>", desc = "Cycle to next buffer"},
+		{"<leader>h", "<cmd>BufferLineCyclePrev<cr>", desc = "Cycle to previous buffer"},
+	},
 }
